3380b410a064b1d9ec14c142f5c8965ff9858c08
ABID Moon <60834531+moonabid@users.noreply.github.com>
2023-10-20 04:15:35 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+   backend "s3" {
+     bucket = "futuiontech-terraform"
+     key    = ".terraform.tfstate"
+     region = "ap-south-1"
+   }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
